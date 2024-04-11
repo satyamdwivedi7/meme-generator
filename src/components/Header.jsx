3a2b78057349cc8bc6b6bf6d55fc3528fdef96eb
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-04-11 19:57:18 +0530

    fixed logo error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
+import logo from "../images/troll-face.png";
 export default function Header() {
   return (
     <header className="header">
-      <img className="header-img" src="/src/images/troll-face.png" />
+      <img className="header-img" src={logo} />
       <h2 className="header-title">Meme Generator</h2>
       <h3 className="header-project">New React Project</h3>
     </header>
